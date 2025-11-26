5a260cd7ed5fc4242fe30ff063a688cb5cd2f958
shreyaajane-lab <shreyaajane@gmail.com>
2025-11-26 16:50:20 +0530

    first commit

---
+++ src/math_ops.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+if __name__ == "__main__":
+    print("Addition:",add(5,3))
+    print("subtraction:",subtract(5,3))
+    print("Multiplication:",multiply(5,3))
+    
